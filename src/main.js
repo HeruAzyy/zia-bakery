691cf070b185f7e6afb0f7bea20542dd45bd601a
Heru Azy <heruazyp@gmail.com>
2023-11-16 11:17:51 +0700

    Fix: perbaikan navbar saat scroll

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("fix-nav");
+  } else {
+    header.classList.remove("fix-nav");
+  }
+};
+
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#navMenu");
 
