679ef074626320c1fda23612775973834998ee8e
Heru Azy <heruazyp@gmail.com>
2023-11-17 01:06:22 +0700

    Fix: Perubahan UI navbar dan About section

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixedNav = header.offsetTop;
+  const header = document.querySelector("header")
+  const fixedNav = header.offsetTop
 
   if (window.scrollY > fixedNav) {
-    header.classList.add("fix-nav");
+    header.classList.add("fix-nav")
   } else {
-    header.classList.remove("fix-nav");
+    header.classList.remove("fix-nav")
   }
-};
+}
 
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector("#navMenu");
+const hamburger = document.querySelector("#hamburger")
+const navMenu = document.querySelector("#navMenu")
 
 hamburger.addEventListener("click", (e) => {
-  navMenu.classList.toggle("active");
-  e.preventDefault();
-});
+  navMenu.classList.toggle("active")
+  e.preventDefault()
+})
