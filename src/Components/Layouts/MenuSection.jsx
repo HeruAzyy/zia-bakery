0bd2f4dc9686af9a69f924d67055bde399943ab3
Heru Azy <heruazyp@gmail.com>
2024-02-21 20:49:48 +0700

    Fix: perbaikan image di menu section

--- src/Components/Layouts/MenuSection.jsx
+++ src/Components/Layouts/MenuSection.jsx
@@ -2,53 +2,53 @@ import { Arrow } from '../../assets/SVG/Arrow'
 import { SectionTitle } from '../Elements/SectionTitle'
 import { CardMenu } from '../Fragments/CardMenu'
 
-const menus = [
-	{
-		id: 1,
-		name: 'Nutella Mille Crepes',
-		image: '../src/assets/images/menu/nutella.jpg',
-		stars: 4.5,
-		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-    voluptatum quos perferendis ipsum libero id.`,
-		price: 'IDR 25.000'
-	},
-	{
-		id: 2,
-		name: 'Matcha Mille Crepes',
-		image: '../src/assets/images/menu/matcha.jpg',
-		stars: 4.3,
-		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-    voluptatum quos perferendis ipsum libero id.`,
-		price: 'IDR 23.000'
-	},
-	{
-		id: 3,
-		name: 'Oreo Mille Crepes',
-		image: '../src/assets/images/menu/oreo.jpg',
-		stars: 4.4,
-		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-    voluptatum quos perferendis ipsum libero id.`,
-		price: 'IDR 20.000'
-	},
-	{
-		id: 4,
-		name: 'Strawberry Mille Crepes',
-		image: '../src/assets/images/menu/strawberry.jpg',
-		stars: 4.6,
-		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-    voluptatum quos perferendis ipsum libero id.`,
-		price: 'IDR 23.000'
-	},
-	{
-		id: 5,
-		name: 'Tiramisu Mille Crepes',
-		image: '../src/assets/images/menu/tiramisu.jpg',
-		stars: 4.7,
-		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-    voluptatum quos perferendis ipsum libero id.`,
-		price: 'IDR 27.000'
-	}
-]
+// const menus = [
+// 	{
+// 		id: 1,
+// 		name: 'Matcha Mille Crepes',
+// 		image: '../src/assets/images/menu/matcha.jpg',
+// 		stars: 4.5,
+// 		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+//     voluptatum quos perferendis ipsum libero id.`,
+// 		price: 'IDR 25.000'
+// 	},
+// 	{
+// 		id: 2,
+// 		name: 'Nutella Mille Crepes',
+// 		image: '../src/assets/images/menu/nutella.jpg',
+// 		stars: 4.5,
+// 		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+//     voluptatum quos perferendis ipsum libero id.`,
+// 		price: 'IDR 23.000'
+// 	},
+// 	{
+// 		id: 3,
+// 		name: 'Oreo Mille Crepes',
+// 		image: '../src/assets/images/menu/oreo.jpg',
+// 		stars: 4.4,
+// 		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+//     voluptatum quos perferendis ipsum libero id.`,
+// 		price: 'IDR 20.000'
+// 	},
+// 	{
+// 		id: 4,
+// 		name: 'Strawberry Mille Crepes',
+// 		image: '../src/assets/images/menu/strawberry.jpg',
+// 		stars: 4.6,
+// 		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+//     voluptatum quos perferendis ipsum libero id.`,
+// 		price: 'IDR 23.000'
+// 	},
+// 	{
+// 		id: 5,
+// 		name: 'Tiramisu Mille Crepes',
+// 		image: '../src/assets/images/menu/tiramisu.jpg',
+// 		stars: 4.7,
+// 		description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+//     voluptatum quos perferendis ipsum libero id.`,
+// 		price: 'IDR 27.000'
+// 	}
+// ]
 
 export const MenuSection = () => {
 	return (
@@ -64,7 +64,7 @@ export const MenuSection = () => {
 			</div>
 
 			<div className='flex gap-5 overflow-x-scroll snap-x snap-mandatory pb-1'>
-				{menus.map((menu) => (
+				{/* {menus.map((menu) => (
 					<CardMenu key={menu.id}>
 						<CardMenu.Header image={menu.image} stars={menu.stars} />
 						<CardMenu.Body name={menu.name}>
@@ -72,7 +72,67 @@ export const MenuSection = () => {
 						</CardMenu.Body>
 						<CardMenu.Footer price={menu.price} />
 					</CardMenu>
-				))}
+				))} */}
+
+				<CardMenu>
+					<CardMenu.Header
+						image='../src/assets/images/menu/matcha.jpg'
+						stars='4.5'
+					/>
+					<CardMenu.Body name='Matcha Mille Crepes'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+						voluptatum quos perferendis ipsum libero id.
+					</CardMenu.Body>
+					<CardMenu.Footer price='23.000' />
+				</CardMenu>
+
+				<CardMenu>
+					<CardMenu.Header
+						image='../src/assets/images/menu/nutella.jpg'
+						stars='4.5'
+					/>
+					<CardMenu.Body name='Nutella Mille Crepes'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+						voluptatum quos perferendis ipsum libero id.
+					</CardMenu.Body>
+					<CardMenu.Footer price='23.000' />
+				</CardMenu>
+
+				<CardMenu>
+					<CardMenu.Header
+						image='../src/assets/images/menu/oreo.jpg'
+						stars='4.5'
+					/>
+					<CardMenu.Body name='Oreo Mille Crepes'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+						voluptatum quos perferendis ipsum libero id.
+					</CardMenu.Body>
+					<CardMenu.Footer price='20.000' />
+				</CardMenu>
+
+				<CardMenu>
+					<CardMenu.Header
+						image='../src/assets/images/menu/strawberry.jpg'
+						stars='4.5'
+					/>
+					<CardMenu.Body name='Strawberry Mille Crepes'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+						voluptatum quos perferendis ipsum libero id.
+					</CardMenu.Body>
+					<CardMenu.Footer price='23.000' />
+				</CardMenu>
+
+				<CardMenu>
+					<CardMenu.Header
+						image='../src/assets/images/menu/tiramisu.jpg'
+						stars='4.5'
+					/>
+					<CardMenu.Body name='Tiramisu Mille Crepes'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+						voluptatum quos perferendis ipsum libero id.
+					</CardMenu.Body>
+					<CardMenu.Footer price='27.000' />
+				</CardMenu>
 			</div>
 		</section>
 	)
