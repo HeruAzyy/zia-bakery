17963d2b75a2e2acc54a0106659664b1e88969d1
Heru Azy <heruazyp@gmail.com>
2024-02-23 12:32:03 +0700

    Add: footer

--- src/Components/Fragments/ContactOrder.jsx
+++ src/Components/Fragments/ContactOrder.jsx
@@ -5,7 +5,7 @@ export const ContactOrder = () => {
 		<div
 			id='contact'
 			className='text-center bg-[url(../images/order-img.webp)] bg-center bg-cover'>
-			<div className='backdrop-brightness-50 bg-dark/50 py-14 text-white'>
+			<div className='backdrop-brightness-50 bg-dark/50 py-20 text-white'>
 				<p>HUBUNGI KAMI UNTUK</p>
 				<h4 className='font-serif font-semibold tracking-wide pb-1'>
 					Online Delivery
@@ -14,6 +14,18 @@ export const ContactOrder = () => {
 				<Button variant='bg-transparant border-2 text-white mx-auto active:scale-100 active:text-black active:bg-white transition-all'>
 					Order Sekarang!
 				</Button>
+
+				<div className='absolute bottom-0 flex items-center justify-between w-full px-5 pb-1 text-sm'>
+					<div className='flex gap-2 items-center'>
+						<img
+							className='w-5 h-5 bg-white rounded p-0.5'
+							src='logo.png'
+							alt='Logo'
+						/>
+						<p className='font-serif tracking-wider'>Zia Bakery.</p>
+					</div>
+					<p className='text-xs'>© 2024 | Developed by Heru Azy.</p>
+				</div>
 			</div>
 		</div>
 	)
