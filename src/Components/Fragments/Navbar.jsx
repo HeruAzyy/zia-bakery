9bf06e35721a1ef968dec448ef47da292eaa8ada
Heru Azy <heruazyp@gmail.com>
2024-02-28 11:46:09 +0700

    Feat: add animation in navbar use Framer Motion

--- src/Components/Fragments/Navbar.jsx
+++ src/Components/Fragments/Navbar.jsx
@@ -6,6 +6,7 @@ import { FaCircleInfo } from 'react-icons/fa6'
 import { RiCustomerService2Fill } from 'react-icons/ri'
 import { BiSolidFoodMenu } from 'react-icons/bi'
 import { MdContactPhone } from 'react-icons/md'
+import { motion } from 'framer-motion'
 
 export const Navbar = () => {
 	let Links = [
@@ -48,7 +49,10 @@ export const Navbar = () => {
 	})
 
 	return (
-		<nav className='shadow-md w-full z-50 fixed top-0 left-0 py-3 px-5 bg-white text-dark lg:px-20'>
+		<motion.nav
+			initial={false}
+			animate={open ? 'open' : 'closed'}
+			className='shadow-md w-full z-50 fixed top-0 left-0 py-3 px-5 bg-white text-dark lg:px-20'>
 			<div className='flex items-center justify-between'>
 				<div>
 					<a href='/'>
@@ -60,6 +64,18 @@ export const Navbar = () => {
 					</a>
 				</div>
 
+				{/* Desktop Nav */}
+				<ul className={`hidden lg:flex lg:gap-7`}>
+					{Links.map((link) => (
+						<li key={link.name} className='text-lg font-semibold'>
+							<a href={link.link} className='hover:text-pink'>
+								{link.name}
+							</a>
+						</li>
+					))}
+				</ul>
+
+				{/* Mobile Nav */}
 				<div
 					ref={menuRef}
 					onClick={() => setOpen(!open)}
@@ -67,22 +83,57 @@ export const Navbar = () => {
 					<CgMenuRightAlt className='w-10 h-10' />
 				</div>
 
-				<ul
-					className={`py-10 px-3 absolute top-[105%] right-0 bg-white/70 shadow-md rounded-l-xl backdrop-blur-lg w-1/2 transition-all duration-500 border-2 border-light md:w-1/4 lg:border-none lg:shadow-none lg:w-auto lg:static lg:flex lg:gap-10 lg:items-center lg:p-0 lg:z-auto ${
-						open ? 'translate-x-0' : 'translate-x-full lg:translate-x-0'
-					}`}>
+				<motion.ul
+					variants={{
+						open: {
+							transform: 'translateX(0px)',
+							transition: {
+								duration: 0.5,
+								delayChildren: 0.3,
+								staggerChildren: 0.05,
+								type: 'spring',
+								stiffness: 300,
+								damping: 20
+							}
+						},
+						closed: {
+							transform: 'translateX(110%)',
+							transition: {
+								duration: 0.2
+							}
+						}
+					}}
+					className='py-5 px-3 absolute top-[110%] right-2 bg-white/70 shadow-md rounded-xl backdrop-blur-lg w-1/2 transition-all border-2 border-light md:w-1/4 lg:hidden'>
 					{Links.map((link) => (
-						<li key={link.name} className='text-left my-2 lg:m-0'>
+						<motion.li
+							variants={{
+								open: {
+									opacity: 1,
+									x: 0,
+									transition: {
+										type: 'spring',
+										stiffness: 300,
+										damping: 20
+									}
+								},
+								closed: {
+									opacity: 0,
+									x: 20,
+									transition: { duration: 0.2 }
+								}
+							}}
+							key={link.name}
+							className='text-left my-2'>
 							<a
 								href={link.link}
-								className='flex items-center gap-3 text-lg font-semibold py-3 px-3 rounded-lg transition-all hover:bg-pink hover:text-white lg:hover:bg-transparent lg:hover:text-pink lg:p-0 lg:rounded-none'>
+								className='flex items-center gap-3 text-lg py-3 px-3 rounded-lg transition-all hover:bg-pink hover:text-white'>
 								{link.icon}
 								{link.name}
 							</a>
-						</li>
+						</motion.li>
 					))}
-				</ul>
+				</motion.ul>
 			</div>
-		</nav>
+		</motion.nav>
 	)
 }
