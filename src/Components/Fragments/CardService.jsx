a52c0629d6710eaba696f063ad281d3d820f4432
Heru Azy <heruazyp@gmail.com>
2024-03-06 17:29:38 +0700

    Style: add hover effect in service section

--- src/Components/Fragments/CardService.jsx
+++ src/Components/Fragments/CardService.jsx
@@ -4,14 +4,16 @@ import { Services } from '../Elements/Services'
 
 export const CardService = ({ title, description }) => {
 	return (
-		<div className='flex flex-col justify-between p-10 border text-center rounded-2xl shadow-md transition-all duration-300 lg:hover:bg-gray-100'>
-			<Services title={title} description={description} />
-			<a
-				href='#services'
-				className='font-semibold inline-flex items-center justify-center gap-2 group'>
-				Selengkapnya
-				<IoArrowForward className='fill-pink h-5 w-5 group-hover:translate-x-1 transition-all' />
-			</a>
+		<div className='bg-light rounded-2xl group transition-all duration-300 lg:hover:-rotate-6'>
+			<div className='flex h-full w-full bg-white flex-col justify-between p-10 border text-center rounded-2xl transition-all duration-300 lg:group-hover:rotate-6 lg:group-hover:shadow-lg'>
+				<Services title={title} description={description} />
+				<a
+					href='#services'
+					className='font-semibold inline-flex items-center justify-center gap-2 group'>
+					Selengkapnya
+					<IoArrowForward className='fill-pink h-5 w-5 group-hover:translate-x-1 transition-all' />
+				</a>
+			</div>
 		</div>
 	)
 }
